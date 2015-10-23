f12f156e97d5fe92f41e629670d32a838ae68de3
Jdway <jdw.davidjaw@gmail.com>
2015-10-24 01:01:16 +0800

    test other way

--- lab1/main.v
+++ lab1/main.v
@@ -64,26 +64,12 @@ always@(posedge clk, posedge rst)
 reg [2:0] q1_cont;
 
 wire [1:0] sw_S, sw_E, sw_N, sw_W;
-wire [1:0] sum;
-assign sum = btnD + btnR + btnU + btnL;
-// sw 只有一組是 00 / 01 / 10 / 11 時才是有效輸出, 否則 warning
-wire argueS, argueE, argueN, argueW;
-assign argueS =  ((( 2'b00 != sw_E ) && ( 2'b00 != sw_N ) && ( 2'b00 != sw_W ) && btnD) || 
-                  (( 2'b00 != sw_S ) && ( 2'b00 != sw_N ) && ( 2'b00 != sw_W ) && btnR) ||
-                  (( 2'b00 != sw_E ) && ( 2'b00 != sw_S ) && ( 2'b00 != sw_W ) && btnU) ||
-                  (( 2'b00 != sw_E ) && ( 2'b00 != sw_N ) && ( 2'b00 != sw_S ) && btnL) )? 1:0;
-assign argueE =  ((( 2'b01 != sw_E ) && ( 2'b01 != sw_N ) && ( 2'b01 != sw_W ) && btnD) || 
-                  (( 2'b01 != sw_S ) && ( 2'b01 != sw_N ) && ( 2'b01 != sw_W ) && btnR) ||
-                  (( 2'b01 != sw_E ) && ( 2'b01 != sw_S ) && ( 2'b01 != sw_W ) && btnU) ||
-                  (( 2'b01 != sw_E ) && ( 2'b01 != sw_N ) && ( 2'b01 != sw_S ) && btnL) )? 1:0;
-assign argueN =  ((( 2'b10 != sw_E ) && ( 2'b10 != sw_N ) && ( 2'b10 != sw_W ) && btnD) || 
-                  (( 2'b10 != sw_S ) && ( 2'b10 != sw_N ) && ( 2'b10 != sw_W ) && btnR) ||
-                  (( 2'b10 != sw_E ) && ( 2'b10 != sw_S ) && ( 2'b10 != sw_W ) && btnU) ||
-                  (( 2'b10 != sw_E ) && ( 2'b10 != sw_N ) && ( 2'b10 != sw_S ) && btnL) )? 1:0;
-assign argueW =  ((( 2'b11 != sw_E ) && ( 2'b11 != sw_N ) && ( 2'b11 != sw_W ) && btnD) || 
-                  (( 2'b11 != sw_S ) && ( 2'b11 != sw_N ) && ( 2'b11 != sw_W ) && btnR) ||
-                  (( 2'b11 != sw_E ) && ( 2'b11 != sw_S ) && ( 2'b11 != sw_W ) && btnU) ||
-                  (( 2'b11 != sw_E ) && ( 2'b11 != sw_N ) && ( 2'b11 != sw_S ) && btnL) )? 1:0;
+wire [2:0] wantS, wantE, wantN, wantW;
+assign wantS = (btnD && (sw_S == 2'b00)) + (btnR && (sw_E == 2'b00)) + (btnU && (sw_N == 2'b00)) + (btnL && (sw_W == 2'b00));
+assign wantE = (btnD && (sw_S == 2'b01)) + (btnR && (sw_E == 2'b01)) + (btnU && (sw_N == 2'b01)) + (btnL && (sw_W == 2'b01));
+assign wantN = (btnD && (sw_S == 2'b10)) + (btnR && (sw_E == 2'b10)) + (btnU && (sw_N == 2'b10)) + (btnL && (sw_W == 2'b10));
+assign wantW = (btnD && (sw_S == 2'b11)) + (btnR && (sw_E == 2'b11)) + (btnU && (sw_N == 2'b11)) + (btnL && (sw_W == 2'b11));
+
 always@(posedge clk, posedge rst)
   if(rst) begin
     q1_state <= 2'b0;
@@ -128,92 +114,14 @@ always@(posedge clk, posedge rst)
           LED <= 8'd0;
       end
       question_3: begin
-        if( btnD && btnR && btnU && btnL) begin
-          LED[0] <= (argueS)? 0 : 1;
-          LED[1] <= (argueS)? 1 : 0;
-          LED[2] <= (argueE)? 0 : 1;
-          LED[3] <= (argueE)? 1 : 0;
-          LED[4] <= (argueN)? 0 : 1;
-          LED[5] <= (argueN)? 1 : 0;
-          LED[6] <= (argueW)? 0 : 1;
-          LED[7] <= (argueW)? 1 : 0;
-        end
-        else if( sum == 2'd1 ) begin
-          case({btnD , btnR , btnU , btnL})
-            4'b0001:
-              LED[sw_W * 2] <= 1;
-            4'b0010:
-              LED[sw_N * 2] <= 1;
-            4'b0100:
-              LED[sw_E * 2] <= 1;
-            4'b1000:
-              LED[sw_S * 2] <= 1;
-          endcase
-        end
-        else if( sum == 2'd2 ) begin
-          case({btnD , btnR , btnU , btnL})
-          // S E N W
-            4'b1100: begin
-              if(sw_S == sw_E)
-                LED[sw_S + 1] <= 1;
-              else begin
-                LED[sw_S] <= 1;
-                LED[sw_E] <= 1;
-              end
-            end
-            4'b0110: begin
-              if(sw_E == sw_N)
-                LED[sw_E + 1] <= 1;
-              else begin
-                LED[sw_E] <= 1;
-                LED[sw_N] <= 1;
-              end
-            end
-            4'b0011: begin
-              if(sw_N == sw_W)
-                LED[sw_N + 1] <= 1;
-              else begin
-                LED[sw_N] <= 1;
-                LED[sw_W] <= 1;
-              end
-            end
-            4'b1001: begin
-              if(sw_S == sw_W)
-                LED[sw_S + 1] <= 1;
-              else begin
-                LED[sw_S] <= 1;
-                LED[sw_W] <= 1;
-              end
-            end
-            4'b1010: begin
-              if(sw_S == sw_N)
-                LED[sw_S + 1] <= 1;
-              else begin
-                LED[sw_S] <= 1;
-                LED[sw_N] <= 1;
-              end
-            end
-            4'b0101: begin
-              if(sw_E == sw_W)
-                LED[sw_E + 1] <= 1;
-              else begin
-                LED[sw_E] <= 1;
-                LED[sw_W] <= 1;
-              end
-            end
-          endcase
-        end
-        else if( sum == 2'd3 ) begin
-          case({btnD , btnR , btnU , btnL})
-          // S E N W
-            4'b1110: begin
-              
-            end
-          endcase
-        end
-        else begin
-          LED <= 8'd0;
-        end
+          LED[0] <= (wantS == 1)? 0 : 1;
+          LED[1] <= (wantS > 1)? 1 : 0;
+          LED[2] <= (wantE == 1)? 0 : 1;
+          LED[3] <= (wantE > 1)? 1 : 0;
+          LED[4] <= (wantN == 1)? 0 : 1;
+          LED[5] <= (wantN > 1)? 1 : 0;
+          LED[6] <= (wantW == 1)? 0 : 1;
+          LED[7] <= (wantW > 1)? 1 : 0;
       end
     endcase
   end
